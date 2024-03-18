4a37ae88413704ccaba8b77866c91a97f1274a58
Ravi Teja Alchuri <raviteja.alchuri@wenable.com>
2024-03-18 15:41:32 -0500

    achievements

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -2,6 +2,9 @@ import ckad from '../assets/achievements/ckad.svg'
 import golang from '../assets/achievements/golang.svg'
 import java from '../assets/achievements/java.svg'
 import entrepreneurship from '../assets/achievements/entrepreneurship.svg'
+import associate from '../assets/achievements/associate.png'
+import professional from '../assets/achievements/professional.png'
+import expert from '../assets/achievements/expert.png'
 
 export const achievementData = {
     bio1 : "As a software engineer with a Master's degree in Computer Science, I have a wealth of knowledge and experience in software engineering. I have worked tirelessly to hone my skills and stay up-to-date with the latest advancements in technology, as evidenced by my numerous certifications.",
@@ -10,16 +13,42 @@ export const achievementData = {
     achievements : [
         {
             id : 1,
+            title : 'Android Enterprise Associate',
+            details : 'Fundamentals of Android Enterprise | Basics of Zero-touch and Android Security | Use cases and success stories of Android Enterprise',
+            date : 'November 1, 2023',
+            field : 'EnterpriseMobility',
+            url : 'https://androidenterprise.exceedlms.com/student/award/ZZY2tErXNfkDnFnUmjDxQUhe',
+            image : associate
+        },
+        {
+            id : 2,
+            title : 'Android Enterprise Professional',
+            details : 'Underpinnings of Android Enterprise | Best practices for deployment | Android security and privacy',
+            date : 'November 2, 2023',
+            field : 'EnterpriseMobility',
+            url : 'https://androidenterprise.exceedlms.com/student/award/4Y2gCTwGQWR1ZcZXuxnRED3d',
+            image : professional
+        },
+        {
+            id : 3,
+            title : 'Android Enterprise Expert',
+            details : 'Implementation and deployment process of Android devices using Android Enterprise | Methods to help in guiding customers through troubleshooting',
+            date : 'November 13, 2023',
+            field : 'EnterpriseMobility',
+            url : 'https://androidenterprise.exceedlms.com/student/award/m4n8nL65WT4283BDxUDtb3XC',
+            image : expert
+        },
+        {
+            id : 4,
             title : 'CKAD: Certified Kubernetes Application Developer',
             details : 'Configuration, Multi-Container Pods, Observability, Pod Design, Services & Networking, State Persistence.',
             date : 'January 27, 2021 - 2024',
             field : 'DevOps',
             url : 'https://www.credly.com/badges/f4bf87b8-9606-4e35-ac32-0b0caeffc067',
             image : ckad
-            
         },
         {
-            id : 2,
+            id : 5,
             title : 'Programming with Google Go',
             details : 'This specialization introduces the Go programming language from Google and provides learners with an overview of Go\'s special features.',
             date : 'October 21, 2020',
@@ -29,7 +58,7 @@ export const achievementData = {
         },
         
         {
-            id : 3,
+            id : 6,
             title : 'Entrepreneurship Specialization',
             details : 'The conception, design, organization, and management of new enterprises.',
             date : 'October 7, 2020',
@@ -39,7 +68,7 @@ export const achievementData = {
         },
 
         {
-            id : 4,
+            id : 7,
             title : 'Oracle Certified Professional',
             details : 'Experience using the Java programming language, understands the basic syntax and structure of the Java programming language and can create Java technology applications that run on server and desktop systems.',
             date : 'September 19, 2015',
